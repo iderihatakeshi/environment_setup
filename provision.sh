962cef19c4bf4351dffb98c633af38cb0dc5a538
iderihatakeshi <ideriha.takeshi@gmail.com>
2016-11-10 22:39:16 +0900

    setup vagrant

---
+++ provision.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sudo yum update -y
+sudo yum -y install gcc gdb make git readline-devel zlib-devel bison flex perl-Test-Harness yum-utils
+
+git clone git://git.postgresql.org/git/postgresql.git
+cd postgresql
+
+./configure --prefix=$HOME/pgsql/master --enable-debug --enable-cassert --enable-tap-tests CFLAGS=-O0
+make
+make install
+
+cd $HOME/pgsql/master
+bin/initdb -D data -E UTF8 --no-locale
+bin/pg_ctl start -D data
